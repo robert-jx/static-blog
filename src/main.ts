5ebb2544fcbdb80616b0a8d921ea981d798c124e
robert-jx <robertjc_c@163.com>
2023-05-16 14:47:46 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+//routes
+import router from "./routes/index";
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
